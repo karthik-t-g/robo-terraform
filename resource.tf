3fadc3d10beb316414ec29ba34e380f4e05a507c
karthik-t-g <karthikeyangopal16@gmail.com>
2025-03-07 17:27:39 +0530

    resource

--- resource.tf
+++ resource.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "frontend" {
   ami = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
+  security_groups = ["sg-01cbddfc072527f97"]
   tags = {
     name="frontend"
   }
